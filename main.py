8af9496bf83205a0867ced4c2148d54936934075
Cem Eteke <ceteke13@ku.edu.tr>
2018-11-22 14:32:15 +0300

    Load parallel

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 from vision_project.data_loader import DataLoader
 import tensorflow as tf, time
 
-dl = DataLoader('/home/ceteke/Documents/datasets/img_align_celeba')
-dl.prepare_images()
-
+dl = DataLoader('/home/ceteke/Documents/datasets/img_align_celeba', n_dataset=1)
+# dl.prepare_images()
+#
 tic = time.time()
 dl.load_dataset()
 toc = time.time()
+#
+dataset = dl.dataset.batch(64)
+
+iterator = dataset.make_one_shot_iterator()
+next_element = iterator.get_next()
+with tf.Session() as sess:
+    print(sess.run(next_element))
 
 print(toc-tic)
-
-print(sum(1 for _ in tf.python_io.tf_record_iterator('data.tfrecord')))
--- vision_project/data_loader.py
+++ vision_project/data_loader.py
@@ -1,12 +1,13 @@
-import numpy as np, os, tensorflow as tf
+import numpy as np, os, tensorflow as tf, threading
 from tqdm import tqdm
 from keras_preprocessing import image
 
 class DataLoader(object):
-    def __init__(self, base_dir):
+    def __init__(self, base_dir, n_dataset=20):
         self.base_dir = base_dir
         self.image_shape = [218, 178, 3]
-        self.tf_record_path = 'data.tfrecord'
+        self.tf_record_prefix = 'data'
+        self.n_dataset = n_dataset
 
     def parser(self, record):
         keys_to_features = {
@@ -19,12 +20,43 @@ class DataLoader(object):
 
         return image
 
+    def prepare_images(self):
+        dirs = os.listdir(self.base_dir)
+        n_imgs = len(dirs)
+        img_per_thread = int(n_imgs/self.n_dataset)
+        threads = []
+
+        for i in range(0, n_imgs, img_per_thread):
+            upper = min(n_imgs, i+img_per_thread)
+            thread_dirs = list(map(lambda x: os.path.join(self.base_dir, x), dirs[i:upper]))
+            data_id = int(i / img_per_thread)
+            data_file = '{}_{}.tfrecord'.format(self.tf_record_prefix, data_id)
+            thread = DataWriter(data_id, thread_dirs, data_file)
+            thread.start()
+            threads.append(thread)
+
+        for t in threads:
+            t.join()
+
     def load_dataset(self):
-        self.dataset = tf.data.TFRecordDataset(self.tf_record_path)
-        self.dataset = self.dataset.map(self.parser, num_parallel_calls=16)
+        for i in range(self.n_dataset):
+            tf_record_path = '{}_{}.tfrecord'.format(self.tf_record_prefix, i)
+            dataset = tf.data.TFRecordDataset(tf_record_path)
+            dataset = dataset.map(self.parser, num_parallel_calls=16)
+            if i == 0:
+                self.dataset = dataset
+            else:
+                self.dataset = self.dataset.concatenate(dataset)
+
+
+class DataWriter(threading.Thread):
+    def __init__(self, id, dirs, tf_record_path):
+        threading.Thread.__init__(self)
+        self.id = id
+        self.dirs = dirs
+        self.tf_record_path = tf_record_path
 
     def load_image(self, dir):
-        dir = os.path.join(self.base_dir, dir)
         img = image.img_to_array(image.load_img(dir))
         return img
 
@@ -35,9 +67,12 @@ class DataLoader(object):
         }))
         writer.write(example.SerializeToString())
 
-    def prepare_images(self):
-        dirs = os.listdir(self.base_dir)
+    def run(self):
+        print("Starting thread: {}".format(self.id))
         with tf.python_io.TFRecordWriter(self.tf_record_path) as writer:
-            for dir in tqdm(dirs):
+            for i, dir in enumerate(self.dirs):
                 img = self.load_image(dir)
                 self.save_tf_record(writer, img)
+                if i % 1000 == 0:
+                    print("Thread {}: {}/{}".format(self.id, i, len(self.dirs)))
+            print("Thread {} done".format(self.id))
